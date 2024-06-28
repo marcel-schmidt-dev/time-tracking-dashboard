41705aa917cd9f028ae12d9776286ebc317800a4
Marcel Schmidt <marcelschmidt1989@live.de>
2024-06-28 09:59:24 +0200

    Fixed Last Bug

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ import data from './data.json' with { type: 'json' };
 const items = document.querySelectorAll('.item');
 const buttons = document.querySelectorAll('.item-1>div:last-child span');
 
-setData('weekly', buttons[1]);
+setData('weekly', buttons[1], "Week");
 
 buttons[0].addEventListener('click', function () {
-    setData('daily', this)
+    setData('daily', this, "Day")
 });
 
 buttons[1].addEventListener('click', function () {
-    setData('weekly', this)
+    setData('weekly', this, "Week")
 });
 
 buttons[2].addEventListener('click', function () {
-    setData('monthly', this)
+    setData('monthly', this, "Month")
 });
 
-function setData(time, btn) {
+function setData(time, btn, last) {
     buttons.forEach(element => {
         element.classList.remove('active');
     });
@@ -24,7 +24,9 @@ function setData(time, btn) {
     data.forEach((element, i) => {
         let ele = items[i].querySelector('.time span');
         ele.innerHTML = element.timeframes[time].current;
-        ele = items[i].querySelector('.last-week span');
+        ele = items[i].querySelector('.last-time span:last-child');
         ele.innerHTML = element.timeframes[time].previous;
+        ele = items[i].querySelector('.last-time span:first-child');
+        ele.innerHTML = last;
     });
 }
